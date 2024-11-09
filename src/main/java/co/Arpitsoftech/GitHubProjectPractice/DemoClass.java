cda94df3eb73c5d47b8b3a3c935319f754045b93
Kartik Kullkarni <kartikkulkarni41.18@gmail.com>
2024-11-09 13:36:41 +0530

    Second Commit

---
+++ src/main/java/co/Arpitsoftech/GitHubProjectPractice/DemoClass.java
@@ -0,0 +1,14 @@
+package co.Arpitsoftech.GitHubProjectPractice;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		
+
+	}
+
+}
